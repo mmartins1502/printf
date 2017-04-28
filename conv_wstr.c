f0afd6b030143c1d6aeaf01c27be167dd83d28fc
Maxime MARTINS <mmartins@e1r4p2.42.fr>
2017-04-28 18:01:45 +0200

    ft_printf manque %p et details

--- conv_wstr.c
+++ conv_wstr.c
@@ -6,7 +6,7 @@
 /*   By: mmartins <mmartins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 15:08:12 by mmartins          #+#    #+#             */
-/*   Updated: 2017/04/27 18:24:03 by mmartins         ###   ########.fr       */
+/*   Updated: 2017/04/28 17:07:17 by mmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 static size_t	ft_wcslen(const wchar_t *wcs)
 {
-	printf("ft_wcslen\n");
 	size_t		converted_char;
 	int			current_char;
 	char		temp[4];
@@ -24,22 +23,36 @@ static size_t	ft_wcslen(const wchar_t *wcs)
 	converted_char = 0;
 	while (*wcs)
 	{
-		printf("ft_wcslen while\n");
 		if ((current_char = ft_wctomb(temp, *wcs++)) == -1)
-			{
-				printf("ft_wcslen if\n");
-				return ((size_t)-1);
-			}
+			return ((size_t)-1);
 		converted_char += current_char;
 		tmp++;
 	}
-	printf("ft_wcslen sortie\n");
+	return (converted_char);
+}
+
+static size_t	ft_wcsnlen(wchar_t *str, size_t n)
+{
+	size_t		converted_char;
+	int			current_char;
+	char		temp[4];
+
+	converted_char = 0;
+	while (str)
+	{
+		if ((current_char = ft_wctomb(temp, *str++)) == -1)
+		{
+			return ((size_t)-1);
+		}
+		if ((converted_char + current_char) > n)
+			break ;
+		converted_char += current_char;
+	}
 	return (converted_char);
 }
 
 static int		ft_width_wstr(char c, int n, t_br *br)
 {
-	printf("ft_width_wstr\n");
 	int		i;
 
 	i = 0;
@@ -51,9 +64,8 @@ static int		ft_width_wstr(char c, int n, t_br *br)
 	return (1);
 }
 
-static int		wc_char(wchar_t x, t_br *br, va_list ap)
+static int		wc_char(wchar_t x, t_br *br, va_list ap, size_t size)
 {
-	printf("wc_char\n");
 	char	*str;
 	int		ret;
 	int		i;
@@ -71,25 +83,26 @@ static int		wc_char(wchar_t x, t_br *br, va_list ap)
 
 int				conv_wstr(t_br *br, va_list ap, t_flag flag)
 {
-	printf("conv_wtr\n");
 	wchar_t		*str;
 	int			i;
 	char		c;
-	int			size;
+	size_t		size;
+	size_t		x;
 
 	i = 0;
 	setlocale(LC_ALL, "");
 	str = va_arg(ap, wchar_t *);
 	if (flag.prec >= 0)
-		size = ft_wcslen(str) < flag.prec ? ft_wcslen(str) : flag.prec;
+		x = ft_wcsnlen(str, flag.prec) < flag.prec ? ft_wcsnlen(str, flag.prec)
+		: flag.prec;
 	else
-		size = ft_wcslen(str);
+		x = ft_wcslen(str);
 	c = flag.zero == 1 ? '0' : ' ';
 	if (flag.width > size && flag.minus == 0)
 		ft_width_wstr(c, flag.width - size, br);
-	while (str[i])
+	while (i < x)
 	{
-		wc_char(str[i], br, ap);
+		wc_char(str[i], br, ap, x);
 		i++;
 	}
 	if (flag.width > size && flag.minus == 1)
